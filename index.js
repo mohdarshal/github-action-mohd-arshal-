72f0d9376213e8b9dac60b7d94ff56ffc1e3ab0b
mohdarshal <mohdarshal10@gmail.com>
2026-09-13 16:55:33 +0530

    Fix typo in message for root endpoint

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).json({ status: "ok", message: "Github Actions"});
+  res.status(200).json({ status: "ok", message: "Github ActionS"});
 });
 
 app.get("/health", (req, res) => {
